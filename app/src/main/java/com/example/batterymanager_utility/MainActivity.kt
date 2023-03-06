9031e86c6ba1230ddcbe3ff38d452be486b1352a
Raiduy <radu.apsan@gmail.com>
2023-03-06 23:16:16 +0100

    activity that outputs batteryManager stats to logcat

--- app/src/main/java/com/example/batterymanager_utility/MainActivity.kt
+++ app/src/main/java/com/example/batterymanager_utility/MainActivity.kt
@@ -1,16 +1,24 @@
 package com.example.batterymanager_utility
 
+import android.content.Context
+import android.content.Context.BATTERY_SERVICE
+import android.os.BatteryManager
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.batterymanager_utility.ui.theme.Batterymanager_utilityTheme
+import java.io.File
+import java.io.FileWriter
+import java.io.IOException
+import java.io.OutputStreamWriter
+import java.lang.reflect.Modifier
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,11 +26,12 @@ class MainActivity : ComponentActivity() {
         setContent {
             Batterymanager_utilityTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+                Surface(color = MaterialTheme.colors.background) {
                     Greeting("Android")
                 }
             }
         }
+        writeFile(this)
     }
 }
 
@@ -31,10 +40,48 @@ fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    Batterymanager_utilityTheme {
-        Greeting("Android")
-    }
+//private fun writeToFile(data: String, context: Context) {
+//    try {
+//        val outputStreamWriter =
+//            OutputStreamWriter(context.openFileOutput("config.txt", Context.MODE_PRIVATE))
+//        outputStreamWriter.write(data)
+//        outputStreamWriter.close()
+//    } catch (e: IOException) {
+//        Log.e("Exception", "File write failed: " + e.toString())
+//    }
+//}
+
+fun writeFile(mcoContext: Context) {
+// write to new file in /data/local/tmp/
+    var batteryManager = mcoContext.getSystemService(BATTERY_SERVICE) as BatteryManager
+    var chargingStatus = batteryManager.isCharging
+    var currentNow = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CURRENT_NOW)
+    Log.e("BatteryMgr", "chargeStat $chargingStatus")
+    Log.e("BatteryMgr", "currentNow $currentNow")
+
+// write to new file in /data/data/com.example.batterymanager_utility/files/mydir
+
+
+
+//    val dir = File(mcoContext.filesDir, "mydir")
+//    if (!dir.exists()) {
+//        dir.mkdir()
+//    }
+//    try {
+//        val gpxfile = File(dir, sFileName)
+//        val writer = FileWriter(gpxfile)
+//        writer.append(sBody)
+//        writer.flush()
+//        writer.close()
+//    } catch (e: Exception) {
+//        e.printStackTrace()
+//    }
 }
+
+//@Preview(showBackground = true)
+//@Composable
+//fun DefaultPreview() {
+//    Batterymanager_utilityTheme {
+//        Greeting("Android", this)
+//    }
+//}
